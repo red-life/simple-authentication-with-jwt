7ff087f830d7b8cdda87da3f42426b4cab077b26
red-life <r3d.l1fe6@gmail.com>
2022-02-03 17:47:31 +0330

    postgres connection added

---
+++ internal/repository/postgres.go
@@ -0,0 +1,18 @@
+package repository
+
+
+import (
+	"fmt"
+	"github.com/red-life/simple-authentication-with-jwt/internal/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func PostgresConnection(cfg *config.PostgresConfig) *gorm.DB{
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tehran", cfg.Host,cfg.Username, cfg.Password, cfg.DB, cfg.Port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil{
+		panic(err)
+	}
+	return db
+}
