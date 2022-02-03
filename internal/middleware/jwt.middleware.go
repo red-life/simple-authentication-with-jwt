85bda303fd59312be38a9ffaff5a1da70c70f3dc
red-life <r3d.l1fe6@gmail.com>
2022-02-03 17:42:54 +0330

    jwt middleware added

--- internal/middleware/jwt.middleware.go
+++ internal/middleware/jwt.middleware.go
@@ -1,1 +1,24 @@
 package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/red-life/simple-authentication-with-jwt/pkg/jwt"
+	"net/http"
+)
+
+func isAuthorized(jwtService jwt.IJWTPackage) gin.HandlerFunc{
+	return func(c *gin.Context) {
+		token, err := c.Cookie("token")
+		if err != nil{
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		_, err = jwtService.CreateToken(token)
+		if err != nil {
+			//context.JSON(http.StatusBadRequest, gin.H{"error": "Missing token", "status": false})
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.Next()
+	}
+}
